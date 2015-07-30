030817f4f26ae117866872154c110f238b03bb15
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-07-30 07:57:30 +0200

    Fixes #11 collector as que

--- collector/index.js
+++ collector/index.js
@@ -6,34 +6,31 @@ var checkMobile = require('./checkMobile');
 var checkHtml = require('./checkHtml');
 var config = require('../config');
 var sites = config.SITES;
-var jobsTotal = 4;
-var jobsDone = 0;
-var pauseTime = 1500;
 
-function areWeDoneYet() {
-  jobsDone ++;
-  if (jobsDone === jobsTotal) {
-    next();
+function checkSite(site) {
+  var jobs = [
+    checkMobile,
+    checkWcag,
+    checkHtml,
+    checkDesktop
+  ];
+  function areWeDoneYet() {
+    if (jobs.length > 0) {
+      var job = jobs.pop();
+      job(site, handleResponse);
+    } else {
+      next();
+    }
   }
-}
-
-function handleResponse(error, message) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(message);
+  function handleResponse(error, message) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(message);
+    }
+    areWeDoneYet();
   }
   areWeDoneYet();
-}
-
-function checkSite() {
-  setTimeout(function delayMe() {
-    var site = sites.pop();
-    checkWcag(site, handleResponse);
-    checkDesktop(site, handleResponse);
-    checkMobile(site, handleResponse);
-    checkHtml(site, handleResponse);
-  }, pauseTime);
 }
 
 function weAreFinished() {
@@ -43,8 +40,8 @@ function weAreFinished() {
 
 function next() {
   if(sites.length > 0) {
-    jobsDone = 0;
-    checkSite();
+    var site = sites.pop();
+    checkSite(site);
   } else {
     weAreFinished();
   }
